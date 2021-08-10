eb1fd1493bbc28b26a24ec3d79f1579170432bd6
tehilaCm <tehilahchasidim@gmail.com>
2021-08-10 15:54:39 +0300

    remove unsave api

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { AiFillStar } from "react-icons/ai";
 
 import actions from "../actions";
-import { savePost, unsavePost } from "../api";
+import { savePost } from "../api";
 import { useAuth } from "../contexts/AuthContext";
 
 import toast from "react-hot-toast";
@@ -93,11 +93,6 @@ const mapDispatchToProps = (dispatch) => {
     },
     unsavePost: async (post, email) => {
       dispatch(actions.unsavePost(post, email));
-      try {
-        await unsavePost({ post, email });
-      } catch (error) {
-        console.log(error);
-      }
     },
   };
 };
